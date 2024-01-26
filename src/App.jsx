f02ab7dcd6b9856f6795ce7e7c386afe1c209bad
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-27 00:57:31 +0600

    Task body header complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 import Banner from './components/banner/Banner';
+import Footer from './components/footer/Footer';
 import NavBar from './components/navbar/NavBar';
+import TaskBody from './components/task-body/TaskBody';
 
 function App() {
   return (
     <div className="bg-[#191D26] font-[Inter] text-white">
       <NavBar></NavBar>
       <Banner></Banner>
+      <TaskBody></TaskBody>
+      <Footer></Footer>
     </div>
   );
 }

8d5e6c7673d5288560d1f267c2a6bcf4f42331d5
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-27 00:38:59 +0600

    Banner section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Banner from './components/banner/Banner';
 import NavBar from './components/navbar/NavBar';
 
 function App() {
   return (
-    <>
+    <div className="bg-[#191D26] font-[Inter] text-white">
       <NavBar></NavBar>
-    </>
+      <Banner></Banner>
+    </div>
   );
 }
 

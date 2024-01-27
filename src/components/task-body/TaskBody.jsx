6b71ee1fcd056e17b54cc296d906dd14822a715a
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-27 23:47:44 +0600

    Task modal added

--- src/components/task-body/TaskBody.jsx
+++ src/components/task-body/TaskBody.jsx
@@ -1,50 +1,44 @@
+import { useState } from 'react';
+import TaskAction from './TaskAction';
+import TaskList from './TaskList';
+import AddTaskModal from './AddTaskModal';
+
 const TaskBody = () => {
+  const defaultTask = [
+    {
+      id: crypto.randomUUID(),
+      title: 'Integration API',
+      description:
+        'Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.',
+      tags: ['Web', 'Python', 'API'],
+      priority: 'High',
+      isFavorite: false,
+    },
+    {
+      id: crypto.randomUUID(),
+      title: 'Integration API',
+      description:
+        'Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.',
+      tags: ['Web', 'Python', 'API'],
+      priority: 'High',
+      isFavorite: true,
+    },
+  ];
+  const [tasks, setTasks] = useState(defaultTask);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  const handleAddTask = () => {
+    setShowAddModal(true);
+  };
   return (
-    <div className="container rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+    <div className="container mb-20 rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+      {showAddModal && <AddTaskModal />}
       <div className="mb-14 items-center justify-between sm:flex">
         <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
-        <div className="flex items-center space-x-5">
-          <form>
-            <div className="flex">
-              <div className="relative overflow-hidden rounded-lg text-gray-50 md:min-w-[380px] lg:min-w-[440px]">
-                <input
-                  type="search"
-                  id="search-dropdown"
-                  className="z-20 block w-full bg-gray-800 px-4 py-2 pr-10 focus:outline-none"
-                  placeholder="Search Task"
-                  required
-                />
-                <button
-                  type="submit"
-                  className="absolute right-2 top-0 h-full rounded-e-lg text-white md:right-4"
-                >
-                  <svg
-                    className="h-4 w-4"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
-                  <span className="sr-only">Search</span>
-                </button>
-              </div>
-            </div>
-          </form>
-          <button className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold">
-            Add Task
-          </button>
-          <button className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold">
-            Delete All
-          </button>
-        </div>
+        <TaskAction handleAddTask={handleAddTask} />
+      </div>
+      <div className="overflow-auto">
+        <TaskList tasks={tasks} />
       </div>
     </div>
   );
